f6df1a10e227b760a6487e301a4c5b385443d015
Débora Vieira <deboraluizafono@gmail.com>
2024-10-13 15:26:53 +0100

    Initial commit

---
+++ src/trabalhocalculadora_deboravieira/TrabalhoCalculadora_DeboraVieira.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabalhocalculadora_deboravieira;
+
+/**
+ *
+ * @author deboravieira
+ */
+public class TrabalhoCalculadora_DeboraVieira {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Calculadora_completa().setVisible(true);
+       
+    }
+    
+}
